c0e92544eee52819dfe665014bac6460985a7210
Marey <kamin_s2@g.hongik.ac.kr>
2022-01-22 13:32:24 +0900

    서브테스크1 통과

--- Yeram/Yutnori/main2.cpp
+++ Yeram/Yutnori/main2.cpp
@@ -19,9 +19,10 @@ struct Space
 
 struct Token
 {
+	bool isActive;
 	char shape;
 	Space* space;
-	Token(char shape, Space* head):shape(shape), space(head) {}
+	Token(char shape, Space* head):shape(shape), space(head), isActive(false){}
 	void moveNext()
 	{
 		space = space->link;
@@ -32,11 +33,15 @@ struct Token
 		if (shape == 'A')
 			return space->pos;
 		else if (shape == 'B')
-			return make_pair(space->pos.first ,space->pos.second + 1);
+			return make_pair(space->pos.first + 1, space->pos.second);
 		else if (shape == 'C')
-			return make_pair(space->pos.first+1, space->pos.second);
+			return make_pair(space->pos.first, space->pos.second + 1);
 		else if (shape == 'D')
 			return make_pair(space->pos.first+1, space->pos.second+1);
+	}
+
+	void setAvtive() {
+		isActive = true;
 	}
 
 };
@@ -52,8 +57,27 @@ void linkSpace(Space* head)
 		temp = node;
 	}
 	//(0,Y)
+	for (int i = 24; 0 <= i; i -= 6)
+	{
+		Space* node = new Space(head, temp, make_pair(i, 0));
+		temp->link = node;
+		temp = node;
+	}
+
 	//(X,0)
+	for (int i = 6; i<= 30; i += 6)
+	{
+		Space* node = new Space(head, temp, make_pair(0, i));
+		temp->link = node;
+		temp = node;
+	}
 	//(X,30)
+	for (int i = 6; i <30; i += 6)
+	{
+		Space* node = new Space(head, temp, make_pair(30,i));
+		temp->link = node;
+		temp = node;
+	}
 	//(X != Y)
 	//(X == Y)
 }
@@ -98,15 +122,23 @@ void drawMap(Space* head, vector<Token*> player)
 
 	Space* linker = head; //for Read
 	
-	string tmp_string(1, player.front()->shape);
-	map[player.front()->getPos().second].replace(player.front()->getPos().first, 1, tmp_string);
+	//setAvtive()
+	for (auto token : player)
+	{
+		string tmp_string(1, token->shape);
+		map[token->getPos().second].replace(token->getPos().first, 1, tmp_string);
+	}
+	
 
 	//뒤에 \n 추가하기.
-	for (auto line : map)
+	vector<string>::iterator itor = map.begin();
+
+	for (; itor != map.end(); itor++)
 	{
-		line.push_back('\n');
+		if(itor == map.end()-1)itor->push_back('\0');
+		else itor->push_back('\n');
 		char* cmap = new char[34];
-		strcpy(cmap, line.c_str());
+		strcpy(cmap, itor->c_str());
 		printf("%s", cmap);//출력
 	}
 }
@@ -136,6 +168,7 @@ int main()
 		}
 
 		if (step == 0) step = 5;
+
 		//가상의 공간에서 말을 생성 및 이동 시킨다.
 		if (player.empty())//말이 하나도 없으면 생성한다.
 		{
@@ -144,10 +177,14 @@ int main()
 
 		while (step != 0)
 		{
+			Token* target = player.front();
 			for (auto token : player)
 			{
-				token->moveNext();
+				if (to_string(token->shape) != turn) continue;
+				target = token;
+				break;
 			}
+			target->moveNext();
 			step--;
 		}
 
